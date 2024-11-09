05751c00b3e77213c082d3e495503916beee84d1
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-11-08 18:40:25 -0700

    create functioning knightMoves functioning
    the function uses breath first traversing for the graph and so far
    works as intended. using the queue data structure im able to look for
    the coordinate im trying to find easier because on each vertex i go
    to i check if im there then return back. I wont be continuing any futher
    because the next vertices are either the same or longer.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import createGraph from "./createGraph.js";
-let all = createGraph();
-console.log(all[3][3]);
+import createGraph from './createGraph.js';
+
+const all = createGraph();
+
+function knightMoves(arrOne, arrtwo) {
+  const q = [];
+  q.push({ vertex: arrOne, pastVertices: [] });
+
+  let front = 0;
+  while (front < q.length) {
+    const currentVertex = q[front].vertex;
+
+    if (JSON.stringify(currentVertex) === JSON.stringify(arrtwo)) {
+      console.log(q[front].pastVertices);
+      console.log(currentVertex);
+      return 'found';
+    }
+    const al = all[currentVertex[0]][currentVertex[1]];
+    const newPastVertices = [...q[front].pastVertices];
+    newPastVertices.push(currentVertex);
+    for (let i = 0; i < al.length; i += 1) {
+      q.push({ vertex: al[i], pastVertices: newPastVertices });
+    }
+
+    front += 1;
+  }
+  return undefined;
+}
+
+knightMoves([0, 0], [7, 3]);
